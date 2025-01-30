f90e98c9e9a4bdb603d4d14c1c0032dd32e38ead
Kirana Alyssa Putri <kiranaalyssa.putri@gmail.com>
2025-01-30 20:54:03 +0700

    Create Top10_NettSales_CabangProvinsi.sql

---
+++ Top10_NettSales_CabangProvinsi.sql
@@ -0,0 +1,20 @@
+-- Top 10 Nett Sales Cabang Provinsi
+SELECT
+    kc.branch_name,
+    kc.provinsi,
+    SUM(ROUND(pd.price * (1 - ft.discount_percentage), 2)) AS total_nett_sales
+FROM
+    `kimia_farma.kf_final_transaction` ft
+JOIN
+    `kimia_farma.kf_product` pd
+ON
+    ft.product_id = pd.product_id
+JOIN
+    `kimia_farma.kf_kantor_cabang` kc
+ON
+    ft.branch_id = kc.branch_id
+GROUP BY
+    kc.branch_name, kc.provinsi
+ORDER BY
+    total_nett_sales DESC
+LIMIT 10;
