364579b93ac1e20633e76db1d6a4a5d57a31501d
Kirana Alyssa Putri <kiranaalyssa.putri@gmail.com>
2025-01-31 22:05:58 +0700

    Update Top10_NettSales_CabangProvinsi.sql

--- Top10_NettSales_CabangProvinsi.sql
+++ Top10_NettSales_CabangProvinsi.sql
@@ -1,6 +1,5 @@
 -- Top 10 Nett Sales Cabang Provinsi
 SELECT
-    kc.branch_name,
     kc.provinsi,
     SUM(ROUND(pd.price * (1 - ft.discount_percentage), 2)) AS total_nett_sales
 FROM
@@ -14,7 +13,7 @@ JOIN
 ON
     ft.branch_id = kc.branch_id
 GROUP BY
-    kc.branch_name, kc.provinsi
+    kc.provinsi
 ORDER BY
     total_nett_sales DESC
 LIMIT 10;
